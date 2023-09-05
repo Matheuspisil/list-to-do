62ec1915b56538013b1d4d9362326c8d93c320c7
Matheuspisil <ajudaserio@yahoo.com.br>
2023-09-05 18:50:37 -0300

    new status

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # list-to-do
+list to do pratico, com alguns lisenings basicos.
+
+evio com enter e ao click
+
+exibição dos concluidos e escluidos em tabela TH
+
+reload reformatado.
+
+aceita qualquer string
+
+otimo para tarefas diarias
+
+
+
--- app.js
+++ app.js
@@ -1,70 +1,68 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const taskInput = document.getElementById("taskInput");
-    const addTaskButton = document.getElementById("addTask");
-    const taskList = document.getElementById("taskList");
-    const completedTasks = document.getElementById("completedTasks");
-    const deletedTasks = document.getElementById("deletedTasks");
+document.addEventListener("DOMContentLoaded", function () {
+  const taskInput = document.getElementById("taskInput");
+  const addTaskButton = document.getElementById("addTask");
+  const taskList = document.getElementById("taskList");
+  const completedTasks = document.getElementById("completedTasks");
+  const deletedTasks = document.getElementById("deletedTasks");
 
-    addTaskButton.addEventListener("click", addTask);
+  addTaskButton.addEventListener("click", addTask);
 
-    taskInput.addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            addTask();
-        }
-    });
+  taskInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
 
-    function addTask() {
-        const taskText = taskInput.value.trim();
-        if (taskText !== "") {
-            const taskRow = createTaskRow(taskText);
-            taskList.appendChild(taskRow);
-            taskInput.value = "";
-        }
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+      const taskRow = createTaskRow(taskText);
+      taskList.appendChild(taskRow);
+      taskInput.value = "";
+    }
+  }
+
+  function createTaskRow(text, withButtons = true) {
+    const taskRow = document.createElement("div");
+    taskRow.className = "task-row";
+
+    const taskTextElement = document.createElement("span");
+    taskTextElement.textContent = text;
+    taskRow.appendChild(taskTextElement);
+
+    if (withButtons) {
+      const completeButton = document.createElement("button");
+      completeButton.textContent = "Concluir";
+      completeButton.className =
+        "complete-button btn btn-success btn-sm d-none";
+      completeButton.addEventListener("click", function () {
+        taskList.removeChild(taskRow);
+        const completedTaskRow = createTaskRow(text, false);
+        completedTasks.appendChild(completedTaskRow);
+      });
+      taskRow.appendChild(completeButton);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Excluir";
+      deleteButton.className = "delete-button btn btn-danger btn-sm d-none";
+      deleteButton.addEventListener("click", function () {
+        taskList.removeChild(taskRow);
+        const deletedTaskRow = createTaskRow(text, false);
+        deletedTasks.appendChild(deletedTaskRow);
+      });
+      taskRow.appendChild(deleteButton);
+
+      taskRow.addEventListener("mouseenter", function () {
+        completeButton.classList.remove("d-none");
+        deleteButton.classList.remove("d-none");
+      });
+
+      taskRow.addEventListener("mouseleave", function () {
+        completeButton.classList.add("d-none");
+        deleteButton.classList.add("d-none");
+      });
     }
 
-    function createTaskRow(text, withButtons = true) {
-        const taskRow = document.createElement("div");
-        taskRow.className = "task-row";
-    
-        const taskTextElement = document.createElement("span");
-        taskTextElement.textContent = text;
-        taskRow.appendChild(taskTextElement);
-    
-        if (withButtons) {
-            const completeButton = document.createElement("button");
-            completeButton.textContent = "Concluir";
-            completeButton.className = "complete-button btn btn-success btn-sm d-none";
-            completeButton.addEventListener("click", function() {
-                taskList.removeChild(taskRow);
-                const completedTaskRow = createTaskRow(text, false); 
-                completedTasks.appendChild(completedTaskRow);
-            });
-            taskRow.appendChild(completeButton);
-    
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Excluir";
-            deleteButton.className = "delete-button btn btn-danger btn-sm d-none";
-            deleteButton.addEventListener("click", function() {
-                taskList.removeChild(taskRow);
-                const deletedTaskRow = createTaskRow(text, false); 
-                deletedTasks.appendChild(deletedTaskRow);
-            });
-            taskRow.appendChild(deleteButton);
-    
-            taskRow.addEventListener("mouseenter", function() {
-                completeButton.classList.remove("d-none");
-                deleteButton.classList.remove("d-none");
-            });
-    
-            taskRow.addEventListener("mouseleave", function() {
-                completeButton.classList.add("d-none");
-                deleteButton.classList.add("d-none");
-            });
-        }
-    
-        return taskRow;
-    }
-    
+    return taskRow;
+  }
 });
-
-
